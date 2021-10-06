5cccbe53f21298a7450115685fca4028b606bb04
brendachirata <43315875+brendachirata@users.noreply.github.com>
2021-10-07 00:51:41 +0200

    Add files via upload

---
+++ mainprog-test.py
@@ -0,0 +1,8 @@
+from mainprog import square_of_x
+
+def testing_square_ofx():
+    assert square_of_x(5)==25
+    print("all is working fine")
+
+if __name__ == '__main__':
+    testing_square_ofx()
---
+++ mainprog.py
@@ -0,0 +1,9 @@
+def square_of_x(x):
+    return x*x
+
+
+def say_hello():
+    print("hello bree bless chira")
+
+if __name__ == '__main__':
+    say_hello()
